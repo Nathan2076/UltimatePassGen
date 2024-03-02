c576f88bab2e6e2676618f8c9cd49b5f1c875de6
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-03-02 20:56:08 -0300

    Separate character types

---
+++ src/Chars.cs
@@ -0,0 +1,10 @@
+namespace UltimatePassGen
+{
+    class Chars
+    {
+        public static readonly char[] uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        public static readonly char[] lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static readonly char[] numbers = "0123456789".ToCharArray();
+        public static readonly char[] symbols = "!@#$%^&*()-_=+[]{}|\\:;\"'<>,.?/´`".ToCharArray();
+    }
+}
